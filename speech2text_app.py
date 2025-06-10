1844102adb5ac31a1bf774f6b54e52d4821ac6ee
kalelei <yk.kaleli@gmail.com>
2025-06-10 14:01:22 +0300

    Summarizing functionality added with LLMA2 model

--- speech2text_app.py
+++ speech2text_app.py
@@ -1,8 +1,52 @@
 import torch
-from transformers import pipeline
+import os
 import gradio as gr
 
-# Function to transcribe audio using the OpenAI Whisper model
+#from langchain.llms import OpenAI
+from langchain.llms import HuggingFaceHub
+
+from transformers import pipeline
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
+from ibm_watson_machine_learning.foundation_models import Model
+from ibm_watson_machine_learning.foundation_models.extensions.langchain import WatsonxLLM
+from ibm_watson_machine_learning.metanames import GenTextParamsMetaNames as GenParams
+
+my_credentials = {
+    "url"    : "https://us-south.ml.cloud.ibm.com"
+}
+params = {
+        GenParams.MAX_NEW_TOKENS: 800, # The maximum number of tokens that the model can generate in a single run.
+        GenParams.TEMPERATURE: 0.1,   # A parameter that controls the randomness of the token generation. A lower value makes the generation more deterministic, while a higher value introduces more randomness.
+    }
+
+LLAMA2_model = Model(
+        model_id= 'meta-llama/llama-3-2-11b-vision-instruct', 
+        credentials=my_credentials,
+        params=params,
+        project_id="skills-network",  
+        )
+
+llm = WatsonxLLM(LLAMA2_model)  
+
+#######------------- Prompt Template-------------####
+
+temp = """
+<s><<SYS>>
+List the key points with details from the context: 
+[INST] The context : {context} [/INST] 
+<</SYS>>
+"""
+
+pt = PromptTemplate(
+    input_variables=["context"],
+    template= temp)
+
+prompt_to_LLAMA2 = LLMChain(llm=llm, prompt=pt)
+
+#######------------- Speech2text-------------####
+
 def transcript_audio(audio_file):
     # Initialize the speech recognition pipeline
     pipe = pipeline(
@@ -11,18 +55,19 @@ def transcript_audio(audio_file):
         chunk_length_s=30,
     )
     # Transcribe the audio file and return the result
-    result = pipe(audio_file, batch_size=8)["text"]
+    transcript_txt = pipe(audio_file, batch_size=8)["text"]
+    result = prompt_to_LLAMA2.run(transcript_txt)
+
     return result
 
-# Set up Gradio interface
-audio_input = gr.Audio(sources="upload", type="filepath")  # Audio input
-output_text = gr.Textbox()  # Text output
+#######------------- Gradio-------------####
 
-# Create the Gradio interface with the function, inputs, and outputs
-iface = gr.Interface(fn=transcript_audio, 
-                     inputs=audio_input, outputs=output_text, 
-                     title="Audio Transcription App",
-                     description="Upload the audio file")
+audio_input = gr.Audio(sources="upload", type="filepath")
+output_text = gr.Textbox()
 
-# Launch the Gradio app
-iface.launch(server_name="127.0.0.1", server_port=7860)
+iface = gr.Interface(fn= transcript_audio, 
+                    inputs= audio_input, outputs= output_text, 
+                    title= "Audio Transcription App",
+                    description= "Upload the audio file")
+
+iface.launch(server_name="0.0.0.0", server_port=7860)
